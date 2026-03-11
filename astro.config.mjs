5ca9d14bc3ee8d36738eb06a5cdf70f66c689ce8
potato <krazypotato19@icloud.com>
2026-03-12 04:35:24 +0800

    version compability

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel';
+import vercel from '@astrojs/vercel/serverless';
 
-// https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: vercel(),
+  adapter: vercel({
+    runtime: 'nodejs18.x'
+  })
 });
